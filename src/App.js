85454fb2054e28532542c95c56a3a00847520584
Taguara Digital <taguaradigital@hotmail.com>
2021-03-02 00:31:27 -0400

    hashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { RestaurantsContextProvider } from './context/RestaurantsContext';
 import Home from './pages/Home';
 import RestaurantDetail from './pages/RestaurantDetail';
