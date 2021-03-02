18077a540b734cbcf02c0b128e03825ff46c7521
Taguara Digital <taguaradigital@hotmail.com>
2021-03-02 00:09:58 -0400

    chapuza

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import RestaurantDetail from './pages/RestaurantDetail';
 import RestaurantUpdate from './pages/RestaurantUpdate';
 
+const homepage = window.location.pathname;
 console.log(window.location.href, 'href')
 console.log(window.location.hostname, 'hostname')
 console.log(window.location.pathname, 'pathname')
@@ -14,9 +15,9 @@ const App = () => {
       <div className="container">
         <Router>
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/restaurants/:id/update" component={RestaurantUpdate} />
-            <Route exact path="/restaurants/:id" component={RestaurantDetail} />
+            <Route exact path={homepage} component={Home} />
+            <Route exact path={homepage + "restaurants/:id/update"} component={RestaurantUpdate} />
+            <Route exact path={homepage + "restaurants/:id"} component={RestaurantDetail} />
             {/* <Route path="/*">
               <Redirect to="/" />
             </Route> */}
