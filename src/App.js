5857479741815725a39caff3cc5fb8929509024a
Taguara Digital <taguaradigital@hotmail.com>
2021-03-01 22:03:36 -0400

    mas pruebas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { RestaurantsContextProvider } from './context/RestaurantsContext';
+import Home from './pages/Home';
+import RestaurantDetail from './pages/RestaurantDetail';
+import RestaurantUpdate from './pages/RestaurantUpdate';
 
 const App = () => {
   return (
     <RestaurantsContextProvider>
-      <Home />
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/restaurants/:id/update" component={RestaurantUpdate} />
+            <Route exact path="/restaurants/:id" component={RestaurantDetail} />
+          </Switch>
+        </Router>
+      </div>
     </RestaurantsContextProvider>
   );
 }
