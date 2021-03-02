16796e94c8bafa493392e536f1a69cab6e5cffc2
Taguara Digital <taguaradigital@hotmail.com>
2021-03-01 23:32:38 -0400

    github

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <div>
             <h1 className="font-weight-light display-1 text-center">
-                Restaurant Finder
+                Restaurant Finder - en Github hosting
             </h1>
         </div>
     )
